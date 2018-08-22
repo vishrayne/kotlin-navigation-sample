1cd15088fa286908fd1f1f450f723b123b5835de
Vishnu Ravi <vishrayne@gmail.com>
2018-08-22 16:25:32 +0530

    prepping nav drawer activity to respect navigation component

--- app/src/main/java/com/example/kotlinnavigationsample/drawernav/DrawerNavigationActivity.kt
+++ app/src/main/java/com/example/kotlinnavigationsample/drawernav/DrawerNavigationActivity.kt
@@ -3,17 +3,18 @@ package com.example.kotlinnavigationsample.drawernav
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.kotlinnavigationsample.R
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_drawer_navigation.drawerLayout
 import kotlinx.android.synthetic.main.activity_drawer_navigation.drawerNavView
 import kotlinx.android.synthetic.main.app_bar_drawer_navigation.toolbar
 
-class DrawerNavigationActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class DrawerNavigationActivity : AppCompatActivity() {
   companion object {
     fun getIntent(context: Context): Intent = Intent(context, DrawerNavigationActivity::class.java)
   }
@@ -23,47 +24,21 @@ class DrawerNavigationActivity : AppCompatActivity(), NavigationView.OnNavigatio
     setContentView(R.layout.activity_drawer_navigation)
     setSupportActionBar(toolbar)
 
-    val toggle = ActionBarDrawerToggle(
-        this, drawerLayout, toolbar, R.string.navigation_drawer_open,
-        R.string.navigation_drawer_close)
-    drawerLayout.addDrawerListener(toggle)
-    toggle.syncState()
-
-    drawerNavView.setNavigationItemSelectedListener(this)
-  }
-
-  override fun onBackPressed() {
-    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-      drawerLayout.closeDrawer(GravityCompat.START)
-    } else {
-      super.onBackPressed()
-    }
-  }
-
-  override fun onNavigationItemSelected(item: MenuItem): Boolean {
-    // Handle navigation view item clicks here.
-    when (item.itemId) {
-      R.id.nav_camera -> {
-        // Handle the camera action
-      }
-      R.id.nav_gallery -> {
-
-      }
-      R.id.nav_slideshow -> {
-
-      }
-      R.id.nav_manage -> {
-
-      }
-      R.id.nav_share -> {
-
-      }
-      R.id.nav_send -> {
-
-      }
+    ActionBarDrawerToggle(
+        this,
+        drawerLayout,
+        toolbar,
+        R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close
+    ).apply {
+      drawerLayout.addDrawerListener(this)
+      syncState()
     }
 
-    drawerLayout.closeDrawer(GravityCompat.START)
-    return true
+    Navigation.findNavController(this, R.id.drawerNavFragment)
+        .also {
+          NavigationUI.setupActionBarWithNavController(this, it, drawerLayout)
+          NavigationUI.setupWithNavController(drawerNavView, it)
+        }
   }
 }
