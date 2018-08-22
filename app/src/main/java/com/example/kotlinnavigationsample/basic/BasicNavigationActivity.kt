ee5af6e3610b925c79bdbd731b9239aa9c6d0fed
Vishnu Ravi <vishrayne@gmail.com>
2018-08-22 12:51:14 +0530

    major refactoring: replacing dummy duplicate fragments with an all-purpose placeholder fragment

--- app/src/main/java/com/example/kotlinnavigationsample/basic/BasicNavigationActivity.kt
+++ app/src/main/java/com/example/kotlinnavigationsample/basic/BasicNavigationActivity.kt
@@ -7,10 +7,14 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlinnavigationsample.R
+import com.example.kotlinnavigationsample.common.PlaceHolderFragment
+import com.example.kotlinnavigationsample.common.PlaceHolderFragment.Action.BACK
+import com.example.kotlinnavigationsample.common.PlaceHolderFragment.Action.NEXT
+import kotlinx.android.synthetic.main.activity_basic_navigation.basicNavHostFragment
 
-class BasicNavigationActivity : AppCompatActivity(), BlankFragment1.OnNextInteractionListener,
-    BlankFragment2.OnBackInteractionListener {
+class BasicNavigationActivity : AppCompatActivity(), PlaceHolderFragment.OnActionListener {
   companion object {
     fun getIntent(context: Context): Intent = Intent(context, BasicNavigationActivity::class.java)
   }
@@ -18,19 +22,38 @@ class BasicNavigationActivity : AppCompatActivity(), BlankFragment1.OnNextIntera
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_basic_navigation)
+
+    // The default layout:navGraph methods doesn't allow us to pass args to the start destination,
+    // so I had to manually inflate the graph with the required args.
+    val navHostFragment = basicNavHostFragment as NavHostFragment
+    navHostFragment.navController.graph =
+        navHostFragment.navController.navInflater.inflate(R.navigation.basic_navigation)
+            .apply {
+              addDefaultArguments(
+                  bundleOf("label" to PlaceHolderFragment.NEXT)
+              )
+            }
   }
 
-  override fun onNext(view: View) {
+  override fun onAction(
+    actionLabel: String,
+    view: View
+  ) = when (actionLabel) {
+    NEXT -> onNext(view)
+    BACK -> onBack(view)
+    else -> throw RuntimeException("Undefined action $actionLabel for PlaceHolderFragment")
+  }
+
+  private fun onNext(view: View) {
     val b = bundleOf(
-        "param1" to 1,
-        "param2" to 2
+        "label" to BACK
     )
 
     Navigation.findNavController(view)
-        .navigate(R.id.action_blankFragment1_to_blankFragment2, b)
+        .navigate(R.id.action_navigation_next_to_navigation_back, b)
   }
 
-  override fun onBack(view: View) {
+  private fun onBack(view: View) {
     Navigation.findNavController(view)
         .navigateUp()
   }
---
+++ app/src/main/java/com/example/kotlinnavigationsample/common/PlaceHolderFragment.kt
@@ -0,0 +1,82 @@
+package com.example.kotlinnavigationsample.common
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kotlinnavigationsample.R
+import kotlinx.android.synthetic.main.fragment_place_holder.actionBtn
+import kotlinx.android.synthetic.main.fragment_place_holder.textView
+import kotlinx.android.synthetic.main.fragment_place_holder.vectorImageView
+
+class PlaceHolderFragment : Fragment() {
+  companion object Action {
+    const val NEXT = "next"
+    const val BACK = "back"
+    const val HOME = "home"
+    const val DASHBOARD = "dashboard"
+    const val NOTIFICATIONS = "notification"
+    const val NONE = "none" // no-op response code
+  }
+
+  interface OnActionListener {
+    fun onAction(
+      actionLabel: String,
+      view: View
+    )
+  }
+
+  private lateinit var listener: OnActionListener
+  private val label: String by lazy { arguments?.getString("label") ?: "" }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_place_holder, container, false)
+  }
+
+  override fun onViewCreated(
+    view: View,
+    savedInstanceState: Bundle?
+  ) {
+    val (tag, iconID, action) = assetPackFor(label)
+    vectorImageView.setImageResource(iconID)
+    textView.text = tag
+
+    if (action == NONE) {
+      actionBtn.visibility = View.GONE
+    } else {
+      actionBtn.visibility = View.VISIBLE
+      attachAction(context, action)
+    }
+  }
+
+  private fun attachAction(
+    context: Context?,
+    action: String
+  ) {
+    if (context !is OnActionListener) {
+      throw RuntimeException(context.toString() + " must implement OnActionListener")
+    }
+
+    listener = context
+    actionBtn.text = action
+    actionBtn.setOnClickListener { listener.onAction(action, it) }
+  }
+
+  private fun assetPackFor(label: String): Triple<String, Int, String> {
+    return when (label) {
+      NEXT -> Triple("Partial#1", R.drawable.ic_home_black_24dp, NEXT)
+      BACK -> Triple("Partial#2", R.drawable.ic_notifications_black_24dp, BACK)
+      HOME -> Triple("Home", R.drawable.ic_home_black_24dp, NONE)
+      DASHBOARD -> Triple("Dashboard", R.drawable.ic_dashboard_black_24dp, NONE)
+      NOTIFICATIONS -> Triple("Notification", R.drawable.ic_notifications_black_24dp, NONE)
+      else -> Triple("Unknown", R.drawable.ic_launcher_background, NONE)
+    }
+  }
+}
