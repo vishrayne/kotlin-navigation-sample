fc2c476dd500dbfca741dbc0a022b6fb6ede7ba9
Vishnu Ravi <vishrayne@gmail.com>
2018-08-28 22:46:22 +0530

    minor refactoring: removing unused imports

--- app/src/main/java/com/example/kotlinnavigationsample/common/MainFragment.kt
+++ app/src/main/java/com/example/kotlinnavigationsample/common/MainFragment.kt
@@ -6,9 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.kotlinnavigationsample.BottomNavigationActivity
-import com.example.kotlinnavigationsample.DeepLinkNavigationActivity
-import com.example.kotlinnavigationsample.DrawerNavigationActivity
 
 import com.example.kotlinnavigationsample.R
 import kotlinx.android.synthetic.main.fragment_main.basicNavButton
