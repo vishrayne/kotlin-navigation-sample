63fa607642131bc13a96ba1b6ae3433c94f7dd29
Vishnu Ravi <vishrayne@gmail.com>
2018-08-31 17:01:45 +0530

    adding stub for safe args example

---
+++ app/src/main/java/com/example/kotlinnavigationsample/SafeArgsNavigationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinnavigationsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SafeArgsNavigationActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_safe_args_navigation)
+  }
+}
